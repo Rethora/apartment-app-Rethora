a592202dc708a5c9a8bca78302e4f1b95e7f76e7
Dane Roelofs <dane.l.roelofs@gmail.com>
2022-04-14 11:57:21 -0700

    Added seed file as well as page to list all apartments

--- app/javascript/components/pages/Apartments.js
+++ app/javascript/components/pages/Apartments.js
@@ -1,10 +1,61 @@
 import React, { Component } from 'react'
-import { NavLink } from 'react-router-dom'
+import {
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  CardImg,
+  CardText
+} from 'reactstrap'
 
 class Apartments extends Component {
   render() {
+    const { apartments } = this.props
+    console.log(apartments)
     return (
-      <h1>Apartments Index</h1>
+      <>
+        <h1>Apartments For Rent</h1>
+        {
+          apartments && apartments.map(apartment => (
+            <Card key={apartment.id}>
+              <CardHeader>
+                {apartment.street}, {apartment.city}, {apartment.state}
+              </CardHeader>
+              <CardBody>
+                <div className='card-flex'>
+                  <div className='card-text-container'>
+                    <CardText>
+                      <strong>Price:</strong> {apartment.price}
+                    </CardText>
+                    <CardText>
+                      <strong>Bedrooms:</strong> {apartment.bedrooms}
+                    </CardText>
+                    <CardText>
+                      <strong>Bathrooms:</strong> {apartment.bathrooms}
+                    </CardText>
+                    <CardText>
+                      <strong>Pets:</strong> {apartment.pets}
+                    </CardText>
+                    <CardText>
+                      <strong>Manager:</strong> {apartment.manager}
+                    </CardText>
+                    <CardText>
+                      <strong>Email:</strong> {apartment.email}
+                    </CardText>
+                  </div>
+                  <CardImg
+                    alt="Card image cap"
+                    src={apartment.image}
+                  />
+                </div>
+              </CardBody>
+              <CardFooter>
+                Listed On: {new Date(apartment.created_at).toLocaleDateString('en-US')}
+              </CardFooter>
+            </Card>
+          ))
+        }
+      </>
     )
   }
 }
